fdbc3d008d48fd0cf9bf60016f5af6a6e22306aa
monajstha <manojstha700@gmail.com>
2025-02-23 15:44:02 +0000

    feat: configure ejs and display messages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import path from "path";
+import indexRouter from "@routes/indexRouter";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + Typescript Server: hello");
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Express + Typescript Server: hello");
+// });
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/routes/indexRouter.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+const indexRouter: Router = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages });
+});
+
+export default indexRouter;
